683001681d6b32f34a76ca400af9e55d8ebceacd
tanmay zalavadiya <tanmayzalavadiya@789.com>
2024-12-20 12:26:45 +0530

    fix the alt issue

--- src/MenuTwo/GoStrezz/GoStrezzImunity.jsx
+++ src/MenuTwo/GoStrezz/GoStrezzImunity.jsx
@@ -25,7 +25,7 @@ export default function GoStrezzImunity() {
       <div className='w-full md:w-[40%]'>
         <img
           src={images[currentImageIndex]}
-          alt={`Image`}
+          alt="Image"
           className='w-full h-auto'
         />
       </div>
