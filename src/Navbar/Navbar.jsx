c900cc77ec5974882b60815463fb3eeec589cb30
tanmay zalavadiya <tanmayzalavadiya@789.com>
2024-12-21 13:16:50 +0530

    updated navbar now

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,51 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./Navbar.css";
 import "./dropdown.css";
-import MenuTwo from "../MenuTwo/MenuTwo"; // Import the MenuTwo component
 import hamburger from "../images/hamburger.png";
 import DeskNav from "./deskNav";
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isHealthDropdownOpen, setIsHealthDropdownOpen] = useState(false);
-  let timeoutId;
-
-  const handleShopMouseEnter = () => {
-    clearTimeout(timeoutId);
-    setIsDropdownOpen(true);
-  };
-
-  const handleShopMouseLeave = () => {
-    timeoutId = setTimeout(() => {
-      setIsDropdownOpen(false);
-    }, 300);
-  };
-
-  const handleHealthMouseEnter = () => {
-    clearTimeout(timeoutId);
-    setIsHealthDropdownOpen(true);
-  };
-
-  const handleHealthMouseLeave = () => {
-    timeoutId = setTimeout(() => {
-      setIsHealthDropdownOpen(false);
-    }, 300);
-  };
 
   const [toggle, setToggle] = useState(false);
-  const handlemenu = () => {
-    setToggle(!toggle);
-    console.log(toggle);
-    
-  };
+  
 
   useEffect(()=>{
     if(window?.innerWidth <= 400){
       setToggle(!toggle);
       
     }
-  },[window.innerWidth])
+  },[window.innerWidth,toggle])
   
   // console.log(window.innerWidth);
 
